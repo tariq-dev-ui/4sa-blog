7c7af7f4321358ebf99729c64f360e1c8093e27d
tariq-dev-ui <tariq778080317t@gmail.com>
2026-02-02 17:03:48 +0300

    add hero banner

--- 4sa-blog/src/app/app.config.ts
+++ 4sa-blog/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideTranslateService } from '@ngx-translate/core';
--- 4sa-blog/src/app/app.ts
+++ 4sa-blog/src/app/app.ts
@@ -2,12 +2,13 @@ import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
+import { Banner } from './layout/banner/banner';
 import { Footer } from './layout/footer/footer';
 import { Navbar } from './layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Banner, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
